868e574ad564eb9126f39a94bebe0ab51d1e1a75
tifus69 <tifus69@hotmail.com>
2019-09-24 19:40:33 +0200

    Add redux

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+import rootReducer from './reducers/rootReducers';
+
+const store = createStore(rootReducer);
 
 function App() {
+  console.log(store);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <p>Salut</p>
+      </div>
+    </Provider>
   );
 }
 
---
+++ src/reducers/rootReducers.js
@@ -0,0 +1,7 @@
+import { combineReducers } from 'redux';
+
+const rootReducer = combineReducers({
+  hello: { 1: 'hello' },
+});
+
+export default rootReducer;
